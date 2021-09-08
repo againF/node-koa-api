6cce8d5e1e8f39b15851bcc8db3e6b537d51f01a
Alan <wyffcld@163.com>
2021-09-08 22:19:20 +0800

    最基础的中间件原理

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const koa = require('koa');
+const Koa = require('koa');
+const { nextTick } = require('process');
+
+const app = new Koa();
+
+function test(ctx,next) {
+    console.log('hello Alan');
+    next();
+}
+
+app.use(test)
+app.use((ctx, next) => {
+    console.log('hello wu');
+    next();
+})
+app.listen(3000)
+console.log('listen 3000');
