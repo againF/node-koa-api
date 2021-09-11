b9c01ae4b40b912988d3cc03067a0d0352a09936
Alan <wyffcld@163.com>
2021-09-11 13:46:25 +0800

    拆分路由

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const Koa = require('koa');
+const book = require('./api/v1/book')
+const classic = require('./api/v1/classic')
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-    console.log('1');
-    await next();
-    console.log('2');
-})
-app.use(async (ctx, next) => {
-    console.log('3');
-    await next();
-    console.log('4');
-})
+app.use(book.routes())
+app.use(classic.routes())
 app.listen(3000)
 console.log('listen 3000');
